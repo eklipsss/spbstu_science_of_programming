a0f78de07517085bbdcac277513e0a4a85ff9106
eklipsss <lips2003@mail.ru>
2023-11-30 19:54:13 +0300

    Add exceptions + CmakeLists.txt
    (?) нужно ли обрабатывать случай, когда wrapper создается некорректно (например, передаваемое количество аргументов не соответствует количеству аргументов функции - incorrect wrapper [1] и [2])

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <conio.h>
 #include "wrapper.h"
 #include "engine.h"
 
 class Subject {
 public:
     int f1(int a, int b) {
-        return a + b;
+        return a - b;
     };
+	int f1_new(int a, int b) {
+		return a * b;
+	};
 	int f2(int a, int b, int c) {
 		return a + b + c;
 	};
@@ -14,20 +18,73 @@ public:
 
 int main() {
 	Subject subj;
-
-	Wrapper wrapper1(&subj, &Subject::f1, {{"arg1", 0}, {"arg2", 0}});
-	Wrapper wrapper2(&subj, &Subject::f2, { {"arg1", 0}, {"arg2", 0}, {"arg3", 0} });
-
-
 	Engine engine;
+	std::cout << "f1(a,b) = a - b " << std::endl;
+	std::cout << "f1_new(a,b) = a * b " << std::endl;
+	std::cout << "f2(a,b,c) = a + b + c " << std::endl << std::endl;
+
+	std::cout << "------------------------"  << std::endl;
+	std::cout << "Correct wrapper: " << std::endl;
+	std::cout << "------------------------" << std::endl;
+	
+	Wrapper wrapper1(&subj, &Subject::f1, { {"arg1", 0}, {"arg2", 0} });
+	Wrapper wrapper1_new(&subj, &Subject::f1_new, { {"arg1", 0}, {"arg2", 0}});
 
 	engine.register_command(&wrapper1, "command1");
+
+	std::cout << "  f1(4,5): " << engine.execute("command1", { {"arg1", 4}, {"arg2", 5} }) << std::endl;
+
+	std::cout << "\nNumber of arguments mismatch: " << std::endl;
+	std::cout << "  f1(4,5,6): " << engine.execute("command1", { {"arg1", 4}, {"arg2", 5}, {"arg3", 6} }) << std::endl;
+
+	std::cout << "\nRandom order of arguments: " << std::endl;
+	std::cout << "  f1(5,4): " << engine.execute("command1", { {"arg2", 4}, {"arg1", 5} }) << std::endl;
+
+	std::cout << "\nUndefined argument name: " << std::endl;
+	std::cout << "  f1(4,-): " << engine.execute("command1", { {"arg1", 4}, {"argument2", 5} }) << std::endl;
+
+	std::cout << "\nTrying to execute a non-existent command: " << std::endl;
+	std::cout << "  f3(4,5): " << engine.execute("command3", { {"arg1", 4}, {"arg2", 5} }) << std::endl;
+
+	std::cout << "\nTrying to register a new command with an existing name: " << std::endl;
+	engine.register_command(&wrapper1_new, "command1");
+
+	std::cout << "\nReset command: " << std::endl;
+	engine.reset_command(&wrapper1_new, "command1");
+	std::cout << "  f1_new(4,5): " << engine.execute("command1", { {"arg1", 4}, {"arg2", 5} }) << std::endl;
+
+	std::cout << "\nReset command with undefined name: " << std::endl;
+	engine.reset_command(&wrapper1, "command_1");
+
+
+	std::cout << std::endl;
+
+	std::cout << "------------------------" << std::endl;
+	std::cout << "Incorrect wrapper [1]: " << std::endl;
+	std::cout << "------------------------" << std::endl;
+	Wrapper wrapper2(&subj, &Subject::f2, { {"arg1", 0}, {"arg2", 0}, {"arg3", 0}, {"arg4", 0} });
+
 	engine.register_command(&wrapper2, "command2");
 
+	std::cout << "f2: " << engine.execute("command2", { {"arg1", 4}, {"arg2", 5}, {"arg3", 6}, {"arg4", 7} }) << std::endl;
 
-	std::cout << "f1: " << engine.execute("command1", {{"arg1", 4}, {"arg2", 5}}) << std::endl;
-	std::cout << "f2: " << engine.execute("command2", { {"arg1", 4}, {"arg2", 5}, {"arg3", 6} }) << std::endl;
+	//std::cout << "------------------------" << std::endl;
+	//std::cout << "Incorrect wrapper [2]: " << std::endl;
+	//std::cout << "------------------------" << std::endl;
+	//Wrapper wrapper3(&subj, &Subject::f2, { {"arg1", 0}, {"arg2", 0} });
 
+	//engine.register_command(&wrapper3, "command3");
+
+	//std::cout << "f3: " << engine.execute("command3", { {"arg1", 4}, {"arg2", 5} }) << std::endl;
+
+	char c;
+	std::cout << "\nPress ENTER to exit " << std::endl;
+	c = _getch();
+	if (c == 13) {
+		std::cout << "\nexit!" << std::endl;
+		exit(0);
+	}
+	system("pause");
 
 	return 0;
 };
--- lab3/wrapper.h
+++ lab3/wrapper.h
@@ -13,16 +13,30 @@ private:
         command = [object, func](std::vector<int>& f_args) { return ((object->*func)(f_args[I]...)); };
     }
 
+    bool is_exist(std::string arg_name, std::unordered_map<std::string, int> const& input_args) {
+        if (input_args.find(arg_name) != input_args.end())
+            return true;
+        return false;
+    }
+
 public:
     template<typename ClassType, typename... Args>
     Wrapper(ClassType* object, int (ClassType::* func)(Args...), std::unordered_map<std::string, int> const& args) : args(args) {
+        //if (sizeof...(Args) != args.size()) //???????????????????
+            //throw std::runtime_error("ERROR: the number of arguments does not match the function arguments");
         set_command(object, func, std::make_index_sequence<sizeof...(Args)>{});
     }
-    int execute(std::unordered_map<std::string, int> const& args) {
+
+    int execute(std::unordered_map<std::string, int> const& input_args) {
         std::vector<int> vec_args;
+        if (input_args.size() != args.size())
+            throw std::runtime_error(" (!) ERROR: the number of input arguments does not match the function arguments");
         for (auto& arg : args) {
-            vec_args.push_back(arg.second);
+            if (!is_exist(arg.first, input_args))
+                throw std::runtime_error(" (!) ERROR: input argument [" + arg.first + "] not found");
+            vec_args.push_back(input_args.find(arg.first)->second);
         }
         return command(vec_args);
-    }    
+    }  
+
 };
